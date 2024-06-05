b737b5bc51c9320e2c6e9c03bb7feee3c6992e44
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-05 09:54:29 +0300

    modified student.py

--- lib/student.py
+++ lib/student.py
@@ -1,3 +1,6 @@
+3 #student.py
+
+
 class Student:
     def hello(self):
         print("Hey there! I'm so excited to learn stuff.")
@@ -6,4 +9,14 @@ class Student:
     # ... (hello method remains the same)
     def raise_hand(self):
         print("Pick me!")
+
+class ChattyStudent(Student):
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Diid you watch The Walking Dead last night? You didn't?! oh man, it was so crazy! what, you don't want any spoilers? Okay well let me just tell you who died...")
+
+
+      def raise_hand(self):
+          for _ in range(10):
+              super().raise_hand()
 

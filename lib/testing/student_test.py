52b06e574ca5cab7a1a83b84255fe6301289a79e
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-05 10:34:13 +0300

    modified student_test.py

--- lib/testing/student_test.py
+++ lib/testing/student_test.py
@@ -21,4 +21,21 @@ class TestChattyStudent(unittest.TestCase):
     def test_chatty_student_hello(Self, mock_stdout):
         chatty_student = ChattyStudent()
         chatty_student.hello()
-        
+        self.assertEqual(mock_stdout.getvalue().strip(), "Hey ther! I'm so excited to learn stuff.")
+
+
+class TestChattyStudent(unittest.TestCase):
+     @patch('sys.stdout', new_callable=StringIO)
+       def test_chatty_student_hello(self, mock_stdout):
+         chatty_student = ChattyStudent()
+         chatty_student.hello()
+         expected_output = """Hey there! I'm so excited to learn stuff.
+  How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? okay well let mejust tell you who died ..."""
+          self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
+
+    @patch('sys.stdout', new_callable=StringIO)
+     def test_chatty_student_raise_hand(self, mock_stdout):
+    chatty_student = ChattyStudent()
+    chatty_student.raise_hand()
+    expected_output = "Pick me!\n" * 10
+    self.assertEqual(mock_stdout.getvalue(), expected_output)

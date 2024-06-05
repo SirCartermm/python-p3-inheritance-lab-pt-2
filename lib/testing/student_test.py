a282b215f38c67f87d24756e351cd6096e8925c9
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-05 10:12:29 +0300

    modified student_test.py

--- lib/testing/student_test.py
+++ lib/testing/student_test.py
@@ -10,4 +10,15 @@ class TestStudent(inittest.TestCase):
         student.hello()
         self.assertEqual(mock_stdout.getvalue().strip(), "Hey ther! I'm so excited to learn stuff.")
 
-    
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_student_raise_hand(self, mock_stdout):
+        student = Student()
+        student.raise_hand()
+        self.assertEqual(mock_stdout.getvalue().strip(), "Pick me!")
+
+class TestChattyStudent(unittest.TestCase):
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_chatty_student_hello(Self, mock_stdout):
+        chatty_student = ChattyStudent()
+        chatty_student.hello()
+        
